331684501316258b1902ad418a9eb86290dcd047
Sourav Sarkar <sourav.sarkar@bookmyshow.com>
2016-08-23 12:14:39 +0530

    added fb and twitter integration to app

--- TwitterApp/TwitterApp/ViewController.m
+++ TwitterApp/TwitterApp/ViewController.m
@@ -14,10 +14,19 @@
 @property (weak, nonatomic) IBOutlet UITextView *tweetTextView;
 
 - (void) configureTweetTextView;
+- (void) showAlertMessage:(NSString *) message;
 
 @end
 
 @implementation ViewController
+
+- (void) showAlertMessage:(NSString *) message {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Twitter App" message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *goAway = [UIAlertAction actionWithTitle:@"Okay" style:UIAlertActionStyleDefault handler:nil];
+    [alertController addAction:goAway];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -34,16 +43,44 @@
     if ([self.tweetTextView isFirstResponder]) {
         [self.tweetTextView resignFirstResponder];
     }
-    
     UIAlertController *actionController = [UIAlertController alertControllerWithTitle:@"Test Title" message:@"Tweet ur note" preferredStyle:UIAlertControllerStyleAlert];
     
     UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:nil];
-    UIAlertAction *tweetAction = [UIAlertAction actionWithTitle:@"Tweet" style:UIAlertActionStyleDefault handler:nil];
-    [actionController addAction:tweetAction];
     
-    [self presentViewController:actionController animated:YES completion:nil];
+    UIAlertAction *tweetAction = [UIAlertAction actionWithTitle:@"Tweet" style:UIAlertActionStyleDefault handler: ^(UIAlertAction *action) {
+        
+        if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
+            SLComposeViewController *twitterVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+              if ([self.tweetTextView.text length] <= 140) {
+                [twitterVC setInitialText:self.tweetTextView.text];
+                  
+              } else {
+                NSString *tweet = [self.tweetTextView.text substringToIndex:140];
+                [twitterVC setInitialText:tweet];
+                // [self showAlertMessage:@"Please Sign In to Twitter"];
+              }
+            [self presentViewController:twitterVC animated:YES completion:nil];
+        } else {
+            [self showAlertMessage:@"Please Sign In to Twitter"];
+        }
+        
+    }];
+    
+    UIAlertAction *facebookAction = [UIAlertAction actionWithTitle:@"Post to FB" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
+        if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
+            SLComposeViewController *facebookVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+            [facebookVC setInitialText:self.tweetTextView.text];
+            [self presentViewController:facebookVC animated:YES completion:nil];
+        } else {
+            [self showAlertMessage:@"Please Sign In to facebook!"];
+        }
+    }];
+        
+    [actionController addAction:tweetAction];
+    [actionController addAction:facebookAction];
     [actionController addAction:cancelAction];
- }
+    [self presentViewController:actionController animated:YES completion:nil];
+}
 
 - (void) configureTweetTextView {
     
